656a4c10c6d371e761eefc030595df3311771825
rxwe <strikebro@mail.ru>
2024-07-31 22:48:24 +0300

    Update Program.cs

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,14 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(c1);
+
+            Console.ReadLine();
         }
     }
 }
